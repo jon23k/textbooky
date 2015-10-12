275f40bd8c7f560884b41ebbc622c6e14b902175
Jonathan Kurian <kurianj@rose-hulman.edu>
2015-10-12 17:04:11 -0400

    Adding things
    things were added. real good.

---
+++ Textbooky/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Textbooky
+//
+//  Created by Jony Kurian on 9/30/15.
+//  Copyright (c) 2015 Jony Kurian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Textbooky/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Textbooky
+//
+//  Created by Jony Kurian on 9/30/15.
+//  Copyright (c) 2015 Jony Kurian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
